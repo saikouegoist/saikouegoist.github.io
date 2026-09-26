cec891223f50c1cff5e547a6e6e311317271a355
meowking <abhi2004que@gmail.com>
2026-09-26 20:41:56 +0530

    added note

--- data/notes.js
+++ data/notes.js
@@ -6,6 +6,13 @@
 
 window.SITE_NOTES = [
   {
+    id: "note-002",
+    date: "2026-09-26",
+    year: "2026",
+    tags: ["thought", "real", "me"],
+    content: "atp i have accepted...that most people like 94.3% - 96.4 people are retarded"
+  },
+  {
     id: "note-001",
     date: "2026-09-18",
     year: "2026",
